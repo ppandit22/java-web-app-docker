a5d40e4b817caec469ef4d613cb79ba89a444354
Pappu Pandit <114795576+ppandit22@users.noreply.github.com>
2024-01-03 16:28:27 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -14,7 +14,7 @@ public class HelloWorldService {
 
 		logger.debug("getDesc() is executed!");
 
-		return "Maven + Spring MVC + Jenkins + Docker Example";
+		return "Hi Everyone";
 
 	}
 
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hello WORLD";
 		}else{
 			return "Hello " + name;
 		}
